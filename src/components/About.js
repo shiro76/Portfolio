86d31631fe23c84a47df5f9719daa61a988b9371
shiro76 <unconfigured@null.spigotmc.org>
2025-09-27 12:42:31 +0200

    Add CV

--- src/components/About.js
+++ src/components/About.js
@@ -24,7 +24,7 @@ export default function About() {
             Me contacter
           </a>
           <a
-            href="/CV_Stanislas.pdf"
+            href="/mon_CV.pdf"
             target="_blank"
             className="bg-gray-200 text-gray-800 px-6 py-3 rounded-lg hover:bg-gray-300 transition"
           >
